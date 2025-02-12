59f32a43ca24925bf01c535a0f5e5914c923eba3
Juan Sebastian Conde <j.tobonconde@gmail.com>
2025-02-12 19:38:42 +0000

    make build

---
+++ index.3559345c.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0},grabCursor:!0,breakpoints:{0:{slidesPerView:1.07,spaceBetween:16},498:{slidesPerView:2.1,spaceBetween:20}}});
+//# sourceMappingURL=index.3559345c.js.map
